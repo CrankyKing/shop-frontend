3f77a79df152c02795b1484ad517571c2c175692
康泽宇 <861863750@qq.com>
2024-08-07 17:39:56 +0800

    home page

--- src/pages/myHome/MyHome.vue
+++ src/pages/myHome/MyHome.vue
@@ -18,7 +18,16 @@
                         <div class="search_text">搜索</div>
                     </div>
                     <div class="classify">
-                        <div class="big_classify"></div>
+                        <div class="big_classify">
+                            
+                            <div v-for="(i, index) in big_classify" :key="index">
+                                <svg class="icon" aria-hidden="true">
+                                    <use :xlink:href="`#${i.icon}`"></use>
+                                </svg>
+                                {{ i.name }}
+                            </div>
+
+                        </div>
                         <div class="small_classify"></div>
                     </div>
                 </div>
@@ -32,17 +41,126 @@
 
 <script>
 import Footer from "../../components/Footer.vue";
-export default {
-    name: '',
-    components: {
-        Footer
-    },
-    data() {
-        return {
+import { reactive, toRefs } from "vue";
+// import Store from "./components/Store.vue";
 
+export default {
+    components: {
+        Footer,
+        // Store,
+    },
+    setup() {
+        let data = reactive({
+            big_classify: [
+                { name: "美食", icon: "icon-apple-and-pear" },
+                { name: "甜点饮品", icon: "icon-tianpin" },
+                { name: "超市便利", icon: "icon-chaoshi" },
+                { name: "生鲜果蔬", icon: "icon-shuiguo" },
+                { name: "滴团买药", icon: "icon-yaopin" },
+            ],
+            small_classify: [
+                { name: "午餐", icon: "icon-iconfonttubiaozhizuo-1" },
+                { name: "买酒", icon: "icon-iconfonttubiaozhizuo-" },
+                { name: "新鲜水果", icon: "icon-iconfonttubiaozhizuo-9" },
+                { name: "汉堡披萨", icon: "icon-iconfonttubiaozhizuo-6" },
+                { name: "休闲饮品", icon: "icon-iconfonttubiaozhizuo-8" },
+                { name: "夜宵", icon: "icon-iconfonttubiaozhizuo-3" },
+                { name: "吐司", icon: "icon-iconfonttubiaozhizuo-5" },
+                { name: "跑腿", icon: "icon-paotuiAPP" },
+                { name: "美人佳丽", icon: "icon-iconfonttubiaozhizuo-2" },
+                { name: "全部分类", icon: "icon-fenlei" },
+            ],
+            centent_nav_list: [
+                {
+                    tab: "天天神券",
+                    data: [
+                        {
+                            pic: "https://img1.baidu.com/it/u=1599947592,1695977044&fm=253&fmt=auto&app=138&f=JPEG?w=640&h=440",
+                            title: "鱼拿酸菜鱼",
+                            sales: "2888",
+                            price: "20",
+                            label: ["门店上新", "很下饭"],
+                        },
+                        {
+                            pic: "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fwww.cfcy168.com%2FUploadFiles%2F2020%2F2%2F15904074889874037.jpg&refer=http%3A%2F%2Fwww.cfcy168.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1645421933&t=66b58fbba9dce6f6b397e38820de24dc",
+                            title: "隆江猪脚饭",
+                            sales: "2888",
+                            price: "20",
+                            label: ["门店上新", "很下饭"],
+                        },
+                        {
+                            pic: "https://img1.baidu.com/it/u=1599947592,1695977044&fm=253&fmt=auto&app=138&f=JPEG?w=640&h=440",
+                            title: "鱼拿酸菜鱼",
+                            sales: "2888",
+                            price: "20",
+                            label: ["门店上新", "很下饭"],
+                        },
+                        {
+                            pic: "https://img1.baidu.com/it/u=1599947592,1695977044&fm=253&fmt=auto&app=138&f=JPEG?w=640&h=440",
+                            title: "鱼拿酸菜鱼",
+                            sales: "2888",
+                            price: "20",
+                            label: ["门店上新", "很下饭"],
+                        },
+                    ],
+                },
+                {
+                    tab: "减配送费",
+                    data: [
+                        {
+                            pic: "https://img1.baidu.com/it/u=1599947592,1695977044&fm=253&fmt=auto&app=138&f=JPEG?w=640&h=440",
+                            title: "鱼拿酸菜鱼",
+                            sales: "2888",
+                            price: "20",
+                            label: ["门店上新", "很下饭"],
+                        },
+                    ],
+                },
+                {
+                    tab: "点评高分",
+                    data: [
+                        {
+                            pic: "https://img1.baidu.com/it/u=1599947592,1695977044&fm=253&fmt=auto&app=138&f=JPEG?w=640&h=440",
+                            title: "鱼拿酸菜鱼",
+                            sales: "2888",
+                            price: "20",
+                            label: ["门店上新", "很下饭"],
+                        },
+                        {
+                            pic: "https://img1.baidu.com/it/u=1599947592,1695977044&fm=253&fmt=auto&app=138&f=JPEG?w=640&h=440",
+                            title: "鱼拿酸菜鱼",
+                            sales: "2888",
+                            price: "20",
+                            label: ["门店上新", "很下饭"],
+                        },
+                    ],
+                },
+                {
+                    tab: "会员满减",
+                    data: [
+                        {
+                            pic: "https://img1.baidu.com/it/u=1599947592,1695977044&fm=253&fmt=auto&app=138&f=JPEG?w=640&h=440",
+                            title: "鱼拿酸菜鱼",
+                            sales: "2888",
+                            price: "20",
+                            label: ["门店上新", "很下饭"],
+                        },
+                        {
+                            pic: "https://img1.baidu.com/it/u=1599947592,1695977044&fm=253&fmt=auto&app=138&f=JPEG?w=640&h=440",
+                            title: "鱼拿酸菜鱼",
+                            sales: "2888",
+                            price: "20",
+                            label: ["门店上新", "很下饭"],
+                        },
+                    ],
+                },
+            ],
+        });
+        return {
+            ...toRefs(data),
         };
-    }
-}
+    },
+};
 </script>
 
 <style scoped lang="less">
@@ -50,6 +168,7 @@ export default {
     display: flex;
     flex-flow: column;
     height: 100%;
+    font-size: 12px;
 
     .content {
         flex: 1;
@@ -99,12 +218,32 @@ export default {
                         font-size: 14px;
                     }
 
-                }
-                input{
-                    width: 100%;
-                    border: 1px soild #ffc400;
+                    input {
+                        width: 100%;
+                        border: 1px soild #ffc400;
+                        border-radius: 20px;
+                        height: 30px;
+                    }
+
                 }
 
+                .classify {
+                    padding: 20px 0;
+
+                    .big_classify {
+                        flex: 1;
+                        display: flex;
+                        justify-content: center;
+                        flex-flow: column;
+                        align-items: center;
+
+                        .icon {
+                            width: 50px;
+                            height: 50px;
+                            margin-bottom: 5px;
+                        }
+                    }
+                }
 
             }
 
